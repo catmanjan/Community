84fee0c7fd0966a9197c5032c402fe437b5eee58
Jan Martin <Jan.Martin@icognition.com.au>
2023-11-15 10:12:48 +1100

    asdf

--- Samples/ServiceAPI/CSharp/GoogleAuthPlugin/GoogleAuthPlugin.cs
+++ Samples/ServiceAPI/CSharp/GoogleAuthPlugin/GoogleAuthPlugin.cs
@@ -2,6 +2,7 @@
 using ServiceStack;
 using ServiceStack.Auth;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace GoogleAuthPlugin
 {
@@ -30,11 +31,8 @@ namespace GoogleAuthPlugin
             appHost.Plugins.Add(new AuthFeature(() => new AuthUserSession(), new IAuthProvider[] {
                          new GoogleAuthProvider(appSettings)
                          {
-                             /*AuthorizeUrl = "https://accounts.google.com/o/oauth2/auth",
-                             AccessTokenUrl = "https://accounts.google.com/o/oauth2/token",
-                             UserProfileUrl = "https://www.googleapis.com/oauth2/v1/userinfo",
-                             VerifyTokenUrl = "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={0}",*/
-                             VerifyAccessTokenAsync = null
+                             AuthorizeUrl = "https://accounts.google.com/o/oauth2/auth?prompt=none",
+                             VerifyAccessTokenAsync = delegate(string a, AuthContext b) { return Task.FromResult(true); }
                          },
                      }, oauthLogin));
         }
