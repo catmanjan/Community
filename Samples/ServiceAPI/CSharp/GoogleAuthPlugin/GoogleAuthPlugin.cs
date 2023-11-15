fcf9e3e9241fd6a0fdbf5244143e5aa9f49e36a9
Jan Martin <Jan.Martin@icognition.com.au>
2023-11-15 11:25:20 +1100

    thankgod

--- Samples/ServiceAPI/CSharp/GoogleAuthPlugin/GoogleAuthPlugin.cs
+++ Samples/ServiceAPI/CSharp/GoogleAuthPlugin/GoogleAuthPlugin.cs
@@ -24,14 +24,9 @@ namespace GoogleAuthPlugin
                 oauthLogin = "~/auth/google";
             }
 
-            /*appHost.Plugins.Add(new AuthFeature(() => new AuthUserSession(), new IAuthProvider[] {
-                        new CustomGoogleOAuth2Provider(appSettings),
-                    }, oauthLogin));*/
-
             appHost.Plugins.Add(new AuthFeature(() => new AuthUserSession(), new IAuthProvider[] {
                          new GoogleAuthProvider(appSettings)
                          {
-                             AuthorizeUrl = "https://accounts.google.com/o/oauth2/auth?prompt=none",
                              VerifyAccessTokenAsync = delegate(string a, AuthContext b) { return Task.FromResult(true); }
                          },
                      }, oauthLogin));
